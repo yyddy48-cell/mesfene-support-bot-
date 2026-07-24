0aa52a9b12a707a459eb27698a1cac0506c482ba
yyddy48-cell <yyddy48@gmail.com>
2026-07-24 11:12:25 +0300

    Update telegram-webhook.js

--- netlify/functions/telegram-webhook.js
+++ netlify/functions/telegram-webhook.js
@@ -30,7 +30,7 @@ const ADMIN_CHAT_ID = process.env.ADMIN_CHAT_ID;
 const ACCOUNT_NUMBER = "1000102646437";
 const DB_URL = "https://chemistry-quiz-b0389-default-rtdb.firebaseio.com";
 
-const PRICE_TABLE = { 1: 50, 2: 80, 3: 125, 4: 160, 5: 210, 6: 260 };
+const PRICE_TABLE = { 1: 50, 2: 70, 3: 90, 4: 110, 5: 120, 6: 140 };
 const MAX_UNITS = 6;
 
 const SUBJECTS = {
